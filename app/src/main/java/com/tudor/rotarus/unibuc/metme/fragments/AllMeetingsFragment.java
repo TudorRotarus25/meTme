7bba7dd22fe3752dcb5064eb5140120fedfbd734
tudor.rotarus <tudor.rotarus@gmail.com>
2016-03-30 00:33:04 +0300

    Added refresh functionality on AllMeetingsFragment

--- app/src/main/java/com/tudor/rotarus/unibuc/metme/fragments/AllMeetingsFragment.java
+++ app/src/main/java/com/tudor/rotarus/unibuc/metme/fragments/AllMeetingsFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -33,6 +34,11 @@ public class AllMeetingsFragment extends Fragment {
     MyApplication app;
 
     ProgressDialog progressDialog;
+    SwipeRefreshLayout refreshLayout;
+    RecyclerView recyclerView;
+
+    private final int INITIAL_CALL = 1;
+    private final int REFRESH_CALL = 2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,16 +65,36 @@ public class AllMeetingsFragment extends Fragment {
         progressDialog.setMessage("Please wait while loading");
         progressDialog.show();
 
-        final RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.fragment_all_meetings_recyclerView);
+        refreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.fragment_all_meetings_swipeRefreshLayout);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                meetingListCall(REFRESH_CALL);
+
+            }
+        });
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.fragment_all_meetings_recyclerView);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
+
+        meetingListCall(INITIAL_CALL);
+    }
+
+    private void meetingListCall(final int callType) {
 
         Call<MeetingsListGetBody> call = app.getRestClient().getApiService().MEETINGS_LIST_GET_BODY_CALL(getPhoneNumber());
         call.enqueue(new Callback<MeetingsListGetBody>() {
             @Override
             public void onResponse(Response<MeetingsListGetBody> response, Retrofit retrofit) {
-                progressDialog.dismiss();
+                if(callType == INITIAL_CALL) {
+                    progressDialog.dismiss();
+                } else {
+                    refreshLayout.setRefreshing(false);
+                }
+
                 if (response.code() == 200 && response.body() != null) {
                     AllMeetingsListAdapter adapter = new AllMeetingsListAdapter(response.body().getMeetings());
                     recyclerView.setAdapter(adapter);
@@ -85,7 +111,6 @@ public class AllMeetingsFragment extends Fragment {
                 Toast.makeText(getActivity(), "Something went wrong. Please try again", Toast.LENGTH_LONG).show();
             }
         });
-
 
     }
 
